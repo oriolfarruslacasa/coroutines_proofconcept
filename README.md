d5b62ef0c68cdf27249b6f7f35f1bac85653a622
Oriol Farrus <oriolfarrus.lacasa@tui.co.uk>
2020-04-08 12:09:13 +0100

    * Added second test for ViewModel and done some cleanup there

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Proof of concept for Coroutines with Retrofit
 Just for self-educational purposes and understand the basic concepts of it.
+Added also Unit Tests for the functions.
--- app/src/test/java/oriol/test/coroutines/ui/main/MainViewModelTest.kt
+++ app/src/test/java/oriol/test/coroutines/ui/main/MainViewModelTest.kt
@@ -36,25 +36,32 @@ class MainViewModelTest {
     @Before
     fun setUp() {
         underTest = MainViewModel(repository, coroutinesTestRule.testDispatcherProvider)
-    }
-
-    @Test
-    fun `will return list of countries`() {
-        val list = listOf(country1, country2, country3)
 
         `when`(runBlocking { repository.listCountries() }).thenReturn(list)
         `when`(runBlocking { repository.getCountryByName(country1.name!!) }).thenReturn(listOf(country1))
         `when`(runBlocking { repository.getCountryByName(country2.name!!) }).thenReturn(listOf(country2))
         `when`(runBlocking { repository.getCountryByName(country3.name!!) }).thenReturn(listOf(country3))
+    }
 
+    @Test
+    fun `will return list of countries in parallel`() {
         underTest.getVariousItems()
 
         assertEquals(list, underTest.liveDataParallel.value)
+    }
+
+    @Test
+    fun `will return list of countries in sequential`() {
+        underTest.liveDataSequential.observeForever {
+            assertEquals(list, it)
+        }
     }
 
     companion object {
         val country1 = Country(name = "countryName1")
         val country2 = Country(name = "countryName2")
         val country3 = Country(name = "countryName3")
+
+        val list = listOf(country1, country2, country3)
     }
 }
